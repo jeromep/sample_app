3bb7a0a4821a6de6a359ab4281d956b8a8a10888
Jerome <jl_penarubia05@yahoo.com>
2012-04-07 18:49:18 +0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,42 @@ require 'spec_helper'
 #    end
 #  end
 #end
-describe "Home page" do
-
+describe "StaticPages" do
+  describe "Home page" do
   it "should have the content 'Sample App'" do
     visit '/static_pages/home'
-    page.should have_content('Sample App')
+    page.should have_selector('h1', :text => 'Sample App')
   end
+  it "should have the right title" do
+    visit '/static_pages/home'
+    page.should have_selector('title',
+                            :title => 'Ruby on Rails Tutorial Sample App | Home')
+  end
+  
+end  
+describe "Help page" do
+      it "should have the content 'Help'" do
+        visit '/static_pages/help'
+        page.should have_selector('h1', :text => 'Help')
+      end
+      it "should have the right title" do
+        visit '/static_pages/help'
+        page.should have_selector('title',
+                                :title => 'Ruby on Rails Tutorial Sample App | Home')
+      end
+      
+    end
+    describe "About page" do
+
+       it "should have the content 'About Us'" do
+         visit '/static_pages/about'
+         page.should have_selector('h1', :text => 'About Us')
+       end
+       it "should have the right title" do
+         visit '/static_pages/about'
+         page.should have_selector('title',
+                                 :title => 'Ruby on Rails Tutorial Sample App | Home')
+       end
+       
+     end
 end
